d2119ab468167dff64cd743c3daaa15f5c79aeb1
Owen.Huang <owen@legalsign.ai>
2024-03-14 14:45:36 +0800

    Fix: some typo

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
 import streamlit as st
 from openai import OpenAI
+import hmac
 
 st.set_page_config(
     page_title="新聞稿產生器",
     page_icon="👋",
 )
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.title("Login")
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
